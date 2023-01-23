e0a40990380e231f3e002c0e1be45e1adce4f703
mvanroessel <mvanroessel@tudelft.nl>
2023-01-23 09:53:33 +0100

    added event listener for the form

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const trackingDiv = document.getElementById('tracking');
 const clicks = 0
 const form = document.getElementById('form');
-trackingDiv.innerText = 'hallo';
 
 document.body.addEventListener('click', (e) => {
     clicks += 1;
 })
 
+form.addEventListener('submit', (e) => {
+    e.defaultPrevented();
+    trackingDiv.innerText('clicks: ' + clicks);
+})
